89a45d923cbed3bd2024c2a1dd507e4d1810f236
Keerat666 <gurkeeratsondhi@gmail.com>
2018-03-03 17:18:36 +0530

    Splitting over

--- app/src/main/java/com/example/keerat666/texus/MainActivity.java
+++ app/src/main/java/com/example/keerat666/texus/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.keerat666.texus;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,8 +18,11 @@ import static com.example.keerat666.texus.R.id.pw;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "";
+
     public void transfer(View view)
     {
+
         EditText editText = (EditText)findViewById(R.id.pw);
         String editTextStr = editText.getText().toString();
         //Toast.makeText(this, ""+editTextStr, Toast.LENGTH_SHORT).show();
@@ -24,10 +32,51 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    public  boolean isReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
 
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Here, thisActivity is the current activity
+      isStoragePermissionGranted();
+      isReadStoragePermissionGranted();
+
     }
 }
--- app/src/main/java/com/example/keerat666/texus/choose.java
+++ app/src/main/java/com/example/keerat666/texus/choose.java
@@ -4,24 +4,88 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import junit.framework.Assert;
+
+
+import org.junit.Test;
+
 import java.net.URISyntaxException;
 
 public class choose extends AppCompatActivity {
     private static final int FILE_SELECT_CODE = 0;
-
+    String fpath="";
     public void uploadmethod(View view)
     {
 
         showFileChooser();
 
     }
+@Test
+    public void splitFile() throws IOException, NoSuchAlgorithmException {
+        String filename1 = fpath;
 
+        File file = new File(filename1);
+   // Toast.makeText(this, "I came here "+file, Toast.LENGTH_SHORT).show();
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        long filesize = file.length();
+        long filesizeActual = 0L;
+        int splitval = 7;
+        int splitsize = (int)(filesize / splitval) + (int)(filesize % splitval);
+        byte[] b = new byte[splitsize];
+        //System.out.println(filename1 + "            " + filesize + " bytes");
+       // Toast.makeText(this, "I came here "+filename1+" size: "+filesize, Toast.LENGTH_SHORT).show();
+
+        try {
+            Toast.makeText(this, "I came here before ", Toast.LENGTH_SHORT).show();
+            fis = new FileInputStream(file);
+            Toast.makeText(this, "I came here after ", Toast.LENGTH_SHORT).show();
+            String name1 = filename1.replaceAll(".mp3", "");
+
+            for (int j = 1; j <= splitval; j++) {
+                String filecalled = name1 + "_split_" + j + ".mp3";
+                fos = new FileOutputStream(filecalled);
+                int i = fis.read(b);
+                fos.write(b, 0, i);
+                fos.close();
+                fos = null;
+                System.out.println(filecalled + "    " + i + " bytes");
+                filesizeActual += i;
+            }
+            Assert.assertEquals(filesize, filesizeActual);
+            //mergeFileParts(filename1, splitval);
+            //check(filename1, mergeFile);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, ""+e, Toast.LENGTH_SHORT).show();
+            Log.i("Error",e+"");
+        }finally {
+            if(fis != null) {
+                fis.close();
+            }
+            if(fos != null) {
+                fos.close();
+            }
+        }
+    Toast.makeText(this, "I came here to display fis"+fis, Toast.LENGTH_SHORT).show();
+
+}
     private void showFileChooser() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
@@ -46,7 +110,7 @@ public class choose extends AppCompatActivity {
                     // Get the Uri of the selected file
                     Uri uri = data.getData();
                     //Log.d(TAG, "File Uri: " + uri.toString());
-                    Toast.makeText(this, "File Uri:"+ uri.toString(), Toast.LENGTH_SHORT).show();
+                   // Toast.makeText(this, "File Uri:"+ uri.toString(), Toast.LENGTH_SHORT).show();
                     // Get the path
                     String path = null;
                     try {
@@ -56,9 +120,18 @@ public class choose extends AppCompatActivity {
                     }
                     //Log.d(TAG, "File Path: " + path);
                     Toast.makeText(this, "File Path:"+ path, Toast.LENGTH_SHORT).show();
+                    fpath=path;
+                    Toast.makeText(this, ""+fpath, Toast.LENGTH_SHORT).show();
                     // Get the file instance
                     // File file = new File(path);
                     // Initiate the upload
+                    try {
+                        splitFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
         }
@@ -86,9 +159,11 @@ public class choose extends AppCompatActivity {
         return null;
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+
     }
 }
